289bf6de03ecc63a6ac59590b4c3922d048cec97
Vinícius Lemos <vinicius.error@gmail.com>
2023-09-27 14:23:19 -0300

    added page change with intent

--- app/src/main/java/com/example/obes/Login.java
+++ app/src/main/java/com/example/obes/Login.java
@@ -3,12 +3,29 @@ package com.example.obes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+    private ImageView button_back_arrow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        this.startComponents();
+
+        button_back_arrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    private void startComponents() {
+        this.button_back_arrow = findViewById(R.id.back_arrow);
     }
 }
--- app/src/main/java/com/example/obes/MainActivity.java
+++ app/src/main/java/com/example/obes/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.obes;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -15,13 +18,28 @@ import com.example.obes.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private Button button_sign_in;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
-        // setContentView(binding.getRoot());
-        setContentView(R.layout.activity_login);
+        setContentView(binding.getRoot());
+        // setContentView(R.layout.activity_login);
+
+        startComponents();
+
+        button_sign_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void startComponents() {
+        this.button_sign_in = findViewById(R.id.sign_in);
     }
 }
