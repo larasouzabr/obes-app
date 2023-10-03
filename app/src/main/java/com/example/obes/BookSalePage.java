496cee79648afff50e48e08fa41bf772a5fc9344
Lara Souza <laragabriellysouzabatista@gmail.com>
2023-10-03 17:54:41 -0300

    created book sale page

---
+++ app/src/main/java/com/example/obes/BookSalePage.java
@@ -0,0 +1,15 @@
+package com.example.obes;
+
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BookSalePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_sale_page);
+}
+}
