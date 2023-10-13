095749593a6a48872d2b421aeb58ef119610e8cf
Lara Souza <laragabriellysouzabatista@gmail.com>
2023-10-13 14:40:36 -0300

    created other crud activities

--- app/src/main/java/com/example/obes/dao/UserCommonDAO.java
+++ app/src/main/java/com/example/obes/dao/UserCommonDAO.java
@@ -27,5 +27,60 @@ public class UserCommonDAO {
         this.listUsersCommon.add(user);
         return true;
     }
+    public boolean deleteUser(UserCommon user) {
+        UserCommon userDelete = null;
+        boolean removed = false;
 
+        for( UserCommon u : listUsersCommon ){
+            if( user.getId() == u.getId() ){
+                userDelete = u;
+                break;
+            }
+        }
+
+        if( userDelete != null ){
+            listUsersCommon.remove( userDelete );
+            removed = true;
+
+        }
+
+        return removed;
+    }
+
+    public boolean editUser(UserCommon user) {
+        UserCommon userEdit = null;
+        boolean edited = false;
+
+        for(UserCommon u : listUsersCommon){
+            if(user.getId() == u.getId()){
+                userEdit = u;
+                edited = true;
+                break;
+            }
+        }
+
+        if(edited){
+            userEdit.setName(user.getName());
+            userEdit.setContact(user.getContact());
+            userEdit.setEmail(user.getEmail());
+            userEdit.setPassword(user.getPassword());
+            userEdit.setCpf(user.getCpf());
+            userEdit.setDateOfBirth(user.getDateOfBirth());
+        }
+
+        return edited;
+    }
+    public UserCommon getUserById(int idUser) {
+        UserCommon user = null;
+
+        for(UserCommon u : this.listUsersCommon){
+            if( u.getId() == idUser ){
+                user = u;
+                break;
+            }
+        }
+
+        return user;
+    }
 }
+
