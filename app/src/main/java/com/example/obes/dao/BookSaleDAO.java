4c79ffdebde3cd7f8db53c7a609cc8b8e30cc5d2
Lara Souza <laragabriellysouzabatista@gmail.com>
2023-10-16 19:13:38 -0300

    created BookSaleDAO

---
+++ app/src/main/java/com/example/obes/dao/BookSaleDAO.java
@@ -0,0 +1,87 @@
+package com.example.obes.dao;
+
+import com.example.obes.model.Book.Book;
+
+import java.util.ArrayList;
+
+public class BookSaleDAO implements IBookDAO {
+    private ArrayList<Book> listBooks;
+    private static BookSaleDAO instance;
+
+    private BookSaleDAO() {
+        this.listBooks = new ArrayList<Book>();
+    }
+
+    public static BookSaleDAO getInstance() {
+        if(instance == null) {
+            instance = new BookSaleDAO();
+        }
+        return instance;
+    }
+
+    public ArrayList<Book> getListBooks() {
+        return listBooks;
+    }
+
+    public boolean addBook(Book book) {
+        this.listBooks.add(book);
+        return true;
+    }
+
+    public boolean deleteBook(Book book) {
+        Book bookDelete = null;
+        boolean removed = false;
+
+        for( Book b : listBooks ){
+            if( book.getId() == b.getId() ){
+                bookDelete = b;
+                break;
+            }
+        }
+
+        if( bookDelete != null ){
+            listBooks.remove( bookDelete );
+            removed = true;
+
+        }
+
+        return removed;
+    }
+
+    public boolean editBook(Book book) {
+        Book bookEdit = null;
+        boolean edited = false;
+
+        for(Book b : listBooks){
+            if(book.getId() == b.getId()){
+                bookEdit = b;
+                edited = true;
+                break;
+            }
+        }
+
+        if(edited){
+            bookEdit.setTitle(book.getTitle());
+            bookEdit.setDescription(book.getDescription());
+            bookEdit.setCategory(book.getCategory());
+            bookEdit.setAvailable(book.getAvailable());
+            bookEdit.setCoverResourceId(book.getCoverResourceId());
+            bookEdit.setPrice(book.getPrice());
+            bookEdit.setCondition(book.getCondition());
+        }
+
+        return edited;
+    }
+    public Book getBookById(int idBook) {
+        Book book = null;
+
+        for(Book b : this.listBooks){
+            if( b.getId() == idBook ){
+                book = b;
+                break;
+            }
+        }
+
+        return book;
+    }
+}
