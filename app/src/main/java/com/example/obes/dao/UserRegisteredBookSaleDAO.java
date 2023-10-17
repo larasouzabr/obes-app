80dfa70c372f9106c96e145b05c940627513c86e
Victor Anthony <anthonyvictor90@gmail.com>
2023-10-16 21:55:12 -0300

    creating relational DAO between user and book sale

---
+++ app/src/main/java/com/example/obes/dao/UserRegisteredBookSaleDAO.java
@@ -0,0 +1,67 @@
+package com.example.obes.dao;
+
+import com.example.obes.model.Book.Book;
+
+import java.util.ArrayList;
+
+public class UserRegisteredBookSaleDAO implements IUserRegisteredBookDAO {
+    private ArrayList<UserBook> listUserBook;
+    private static UserRegisteredBookSaleDAO instance;
+
+    private UserRegisteredBookSaleDAO() {
+        this.listUserBook = new ArrayList<UserBook>();
+    }
+
+    public static UserRegisteredBookSaleDAO getInstance() {
+        if(instance == null) {
+            instance = new UserRegisteredBookSaleDAO();
+        }
+        return instance;
+    }
+
+    public ArrayList<UserBook> getListUserBook() {
+        return this.listUserBook;
+    }
+
+    public boolean addUserBook(int id_user, int id_book) {
+        UserBook userBook = new UserBook(id_user, id_book);
+
+        this.listUserBook.add(userBook);
+
+        return true;
+    }
+
+    public boolean deleteUserBook(int id_user, int id_book) {
+        UserBook userBookDeleted = null;
+        boolean removed = false;
+
+        for(UserBook userBook : this.listUserBook) {
+            if(userBook.getId_user() == id_user && userBook.getId_book() == id_book) {
+                userBookDeleted = userBook;
+                break;
+            }
+        }
+
+        if(userBookDeleted != null) {
+            this.listUserBook.remove(userBookDeleted);
+            removed = true;
+        }
+
+        return removed;
+    }
+
+    public ArrayList<Book> getListBookByUser(int id_user) {
+        BookSaleDAO bookSaleDAO = BookSaleDAO.getInstance();
+        BookDAO bookDonateDAO = BookDAO.getInstance();
+
+        ArrayList<Book> listBooks = new ArrayList<Book>();
+
+        for (UserBook userBook : this.listUserBook) {
+            if (userBook.getId_user() == id_user) {
+                listBooks.add(bookSaleDAO.getBookById(userBook.getId_book()));
+            }
+        }
+
+        return listBooks;
+    }
+}
