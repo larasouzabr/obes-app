f421f2a7a5cd5828d31bc241c85d3377c8f5d887
Vinícius Lemos <vinicius.error@gmail.com>
2023-10-17 12:03:52 -0300

    created relational DAO between User and BookDonate

---
+++ app/src/main/java/com/example/obes/dao/UserRegisteredBookDonateDAO.java
@@ -0,0 +1,66 @@
+package com.example.obes.dao;
+
+import com.example.obes.model.Book.Book;
+
+import java.util.ArrayList;
+
+public class UserRegisteredBookDonateDAO implements IUserRegisteredBookDAO {
+    private ArrayList<UserBook> listUserBook;
+    private static UserRegisteredBookDonateDAO instance;
+
+    private UserRegisteredBookDonateDAO() {
+        this.listUserBook = new ArrayList<UserBook>();
+    }
+
+    public static UserRegisteredBookDonateDAO getInstance() {
+        if(instance == null) {
+            instance = new UserRegisteredBookDonateDAO();
+        }
+        return instance;
+    }
+
+    public ArrayList<UserBook> getListUserBook() {
+        return listUserBook;
+    }
+
+    public boolean addUserBook(int id_user, int id_book) {
+        UserBook userBook = new UserBook(id_user, id_book);
+
+        listUserBook.add(userBook);
+
+        return true;
+    }
+
+    public boolean deleteUserBook(int id_user, int id_book) {
+        UserBook userBookDeleted = null;
+        boolean removed = false;
+
+        for(UserBook userBook : this.listUserBook) {
+            if(userBook.getId_user() == id_user && userBook.getId_book() == id_book) {
+                userBookDeleted = userBook;
+                break;
+            }
+        }
+
+        if(userBookDeleted != null) {
+            this.listUserBook.remove(userBookDeleted);
+            removed = true;
+        }
+
+        return removed;
+    }
+
+    public ArrayList<Book> getListBookByUser(int id_user) {
+        BookDAO bookDAO = BookDAO.getInstance();
+
+        ArrayList<Book> listBooks = new ArrayList<Book>();
+
+        for (UserBook userBook : this.listUserBook) {
+            if (userBook.getId_user() == id_user) {
+                listBooks.add(bookDAO.getBookById(userBook.getId_book()));
+            }
+        }
+
+        return listBooks;
+    }
+}
