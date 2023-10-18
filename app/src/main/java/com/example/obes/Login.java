ccce5d3372a228f3901bcbb0d655c73435a9e9c9
Vinícius Lemos <vinicius.error@gmail.com>
2023-10-18 12:42:14 -0300

    created login management session

--- app/src/main/java/com/example/obes/Login.java
+++ app/src/main/java/com/example/obes/Login.java
@@ -6,14 +6,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.obes.dao.LoginSessionManager;
+import com.example.obes.dao.UserCommonDAO;
+import com.example.obes.dao.UserInstitutionalDAO;
+import com.example.obes.model.User.UserCommon;
+import com.example.obes.model.User.UserInstitutional;
+
+import java.util.ArrayList;
 
 public class Login extends AppCompatActivity {
     private ImageView button_back_arrow;
     private TextView text_register;
-
     private Button button_login;
+    private EditText etUserEmail;
+    private EditText etUserPassword;
+    private LoginSessionManager loginSessionManager = LoginSessionManager.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,8 +52,21 @@ public class Login extends AppCompatActivity {
         this.button_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Login.this, HomePage.class);
-                startActivity(intent);
+                String email = etUserEmail.getText().toString();
+                String password = etUserPassword.getText().toString();
+
+                if (email.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(Login.this, "Por favor, preencha todos os campos", Toast.LENGTH_SHORT).show();
+                } else {
+                    boolean isCheckedLogin = checkedLogin(email, password);
+
+                    if (isCheckedLogin) {
+                        Intent intent = new Intent(Login.this, HomePage.class);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(Login.this, "E-mail/Senha inválidos", Toast.LENGTH_SHORT).show();
+                    }
+                }
             }
         });
     }
@@ -50,5 +75,35 @@ public class Login extends AppCompatActivity {
         this.button_back_arrow = findViewById(R.id.back_arrow);
         this.text_register = findViewById(R.id.register);
         this.button_login = findViewById(R.id.login);
+        this.etUserEmail = findViewById(R.id.user_email);
+        this.etUserPassword = findViewById(R.id.user_password);
+    }
+
+    private boolean checkedLogin(String email, String password) {
+        UserCommonDAO userCommonDAO = UserCommonDAO.getInstance();
+        UserInstitutionalDAO userInstitutionalDAO = UserInstitutionalDAO.getInstance();
+
+        ArrayList<UserCommon> usersCommon = userCommonDAO.getListUsers();
+        ArrayList<UserInstitutional> usersInstitutional = userInstitutionalDAO.getListUsers();
+
+        for (UserCommon user : usersCommon) {
+            if (user.getEmail().equals(email)) {
+                if (user.getPassword().equals(password)) {
+                    loginSessionManager.loginUserCommon(user);
+                    return true;
+                }
+            }
+        }
+
+        for (UserInstitutional user : usersInstitutional) {
+            if (user.getEmail().equals(email)) {
+                if (user.getPassword().equals(password)) {
+                    loginSessionManager.loginUserInstitutional(user);
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 }
