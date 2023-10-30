2c94b80de95f5dc6e11f9cbdb06c0c099fabcb0e
Vinícius Lemos <vinicius.error@gmail.com>
2023-10-30 18:46:22 -0300

    created perfil page userInstitutional

---
+++ app/src/main/java/com/example/obes/perfil/PerfilUserInstitutional.java
@@ -0,0 +1,16 @@
+package com.example.obes.perfil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.obes.R;
+
+public class PerfilUserInstitutional extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_user_institutional);
+    }
+}
