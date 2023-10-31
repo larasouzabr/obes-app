9280ba4035448f5ebe6ed92b850a20ffec915e08
Vinícius Lemos <vinicius.error@gmail.com>
2023-10-31 10:39:13 -0300

    flow to add user data on information page

--- app/src/main/java/com/example/obes/InformationUserPage.java
+++ app/src/main/java/com/example/obes/InformationUserPage.java
@@ -3,12 +3,49 @@ package com.example.obes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.obes.dao.LoginSessionManager;
+import com.example.obes.model.User.UserCommon;
 
 public class InformationUserPage extends AppCompatActivity {
-
+    private TextView tvTitlePage;
+    private EditText etDate;
+    private EditText etCpf;
+    private EditText etPhone;
+    private Button button_cancel;
+    private LoginSessionManager loginSessionManager;
+    private UserCommon userLogged;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information_user_page);
+        this.startComponents();
+
+        this.userLogged = loginSessionManager.getCurrentUserCommon();
+
+        this.tvTitlePage.setText("Confirme seus dados");
+
+        this.etDate.setText(this.userLogged.getDateOfBirth());
+        this.etCpf.setText(this.userLogged.getCpf());
+        this.etPhone.setText(this.userLogged.getContact());
+
+        this.button_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+    private void startComponents() {
+        this.tvTitlePage = findViewById(R.id.title_page);
+        this.etDate = findViewById(R.id.date);
+        this.etCpf = findViewById(R.id.cpf);
+        this.etPhone = findViewById(R.id.phone);
+        this.button_cancel = findViewById(R.id.button_cancel);
+        this.loginSessionManager = LoginSessionManager.getInstance();
     }
 }
